7894b92c8609158a9493e8b9ab0f1d568500a979
ixnp <enprocopios@gmail.com>
2019-11-16 20:07:16 -0800

    server

--- server/index.js
+++ server/index.js
@@ -4,6 +4,11 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
 const router = require('./router');
+const mongoose = require('mongoose')
+
+//DB Setup
+
+mongoose.connect('mongodb://localhost:auth/auth')
 //App Set up
 app.use(morgan('combined'));
 app.use(bodyParser.json({type:'*/*'}));
